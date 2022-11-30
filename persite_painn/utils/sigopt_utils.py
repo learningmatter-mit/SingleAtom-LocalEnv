d58784750eee68d4d8b80a149c01b67733c71a91
hojechun <hojechun@mit.edu>
2022-11-30 06:28:32 -0500

    Multifidelity bug

--- persite_painn/utils/sigopt_utils.py
+++ persite_painn/utils/sigopt_utils.py
@@ -1,34 +1,36 @@
-def convert_to_sigopt_params(details, modelparams, sigoptparams):
+def convert_to_sigopt_params(details, modelparams, sigoptparams, fidelity=False):
     converted_params = {}
     converted_details = details
+    if fidelity:
+        sigoptparams.setdefault(
+            "atom_fea_len_fidelity", modelparams["atom_fea_len"]["fidelity"]
+        )
+        sigoptparams.setdefault("h_fea_len_fidelity", modelparams["h_fea_len"]["fidelity"])
+        sigoptparams.setdefault("n_h_fidelity", modelparams["n_h"]["fidelity"])
+        sigoptparams.setdefault(
+            "readout_dropout_fidelity", modelparams["readout_dropout"]["fidelity"]
+        )
+        sigoptparams.setdefault(
+            "fc_dropout_fidelity", modelparams["fc_dropout"]["fidelity"]
+        )
+        sigoptparams.setdefault(
+            "loss_coeff_fidelity", modelparams["loss_coeff"]["fidelity"]
+        )
+
     sigoptparams.setdefault("feat_dim", modelparams["feat_dim"])
     sigoptparams.setdefault("n_rbf", modelparams["n_rbf"])
     sigoptparams.setdefault("cutoff", modelparams["cutoff"])
     sigoptparams.setdefault("num_conv", modelparams["num_conv"])
     sigoptparams.setdefault("conv_dropout", modelparams["conv_dropout"])
     sigoptparams.setdefault(
-        "atom_fea_len_fidelity", modelparams["atom_fea_len"]["fidelity"]
-    )
-    sigoptparams.setdefault(
         "atom_fea_len_target", modelparams["atom_fea_len"]["target"]
     )
-    sigoptparams.setdefault("h_fea_len_fidelity", modelparams["h_fea_len"]["fidelity"])
     sigoptparams.setdefault("h_fea_len_target", modelparams["h_fea_len"]["target"])
-    sigoptparams.setdefault("n_h_fidelity", modelparams["n_h"]["fidelity"])
     sigoptparams.setdefault("n_h_target", modelparams["n_h"]["target"])
-    sigoptparams.setdefault(
-        "readout_dropout_fidelity", modelparams["readout_dropout"]["fidelity"]
-    )
     sigoptparams.setdefault(
         "readout_dropout_target", modelparams["readout_dropout"]["target"]
     )
-    sigoptparams.setdefault(
-        "fc_dropout_fidelity", modelparams["fc_dropout"]["fidelity"]
-    )
     sigoptparams.setdefault("fc_dropout_target", modelparams["fc_dropout"]["target"])
-    sigoptparams.setdefault(
-        "loss_coeff_fidelity", modelparams["loss_coeff"]["fidelity"]
-    )
     sigoptparams.setdefault("loss_coeff_target", modelparams["loss_coeff"]["target"])
 
     # details
@@ -45,30 +47,47 @@ def convert_to_sigopt_params(details, modelparams, sigoptparams):
     converted_params["n_rbf"] = sigoptparams.n_rbf
     converted_params["num_conv"] = sigoptparams.num_conv
     converted_params["conv_dropout"] = sigoptparams.conv_dropout
-    converted_params["atom_fea_len"] = {
-        "target": sigoptparams.atom_fea_len_target,
-        "fidelity": sigoptparams.atom_fea_len_fidelity,
-    }
-    converted_params["h_fea_len"] = {
-        "target": sigoptparams.h_fea_len_target,
-        "fidelity": sigoptparams.h_fea_len_fidelity,
-    }
-    converted_params["n_h"] = {
-        "target": sigoptparams.n_h_target,
-        "fidelity": sigoptparams.n_h_fidelity,
-    }
-    converted_params["readout_dropout"] = {
-        "target": sigoptparams.readout_dropout_target,
-        "fidelity": sigoptparams.readout_dropout_fidelity,
-    }
-    converted_params["fc_dropout"] = {
-        "target": sigoptparams.fc_dropout_target,
-        "fidelity": sigoptparams.fc_dropout_fidelity,
-    }
-    converted_params["loss_coeff"] = {
-        "target": sigoptparams.loss_coeff_target,
-        "fidelity": sigoptparams.loss_coeff_fidelity,
-    }
+    if fidelity:
+        converted_params["atom_fea_len"] = {
+            "target": sigoptparams.atom_fea_len_target,
+            "fidelity": sigoptparams.atom_fea_len_fidelity,
+        }
+        converted_params["h_fea_len"] = {
+            "target": sigoptparams.h_fea_len_target,
+            "fidelity": sigoptparams.h_fea_len_fidelity,
+        }
+        converted_params["n_h"] = {
+            "target": sigoptparams.n_h_target,
+            "fidelity": sigoptparams.n_h_fidelity,
+        }
+        converted_params["readout_dropout"] = {
+            "target": sigoptparams.readout_dropout_target,
+            "fidelity": sigoptparams.readout_dropout_fidelity,
+        }
+        converted_params["fc_dropout"] = {
+            "target": sigoptparams.fc_dropout_target,
+            "fidelity": sigoptparams.fc_dropout_fidelity,
+        }
+        converted_params["loss_coeff"] = {
+            "target": sigoptparams.loss_coeff_target,
+            "fidelity": sigoptparams.loss_coeff_fidelity,
+        }
+    else:
+        converted_params["atom_fea_len"] = {
+            "target": sigoptparams.atom_fea_len_target,
+        }
+        converted_params["h_fea_len"] = {
+            "target": sigoptparams.h_fea_len_target,
+        }
+        converted_params["n_h"] = {
+            "target": sigoptparams.n_h_target,
+        }
+        converted_params["readout_dropout"] = {
+            "target": sigoptparams.readout_dropout_target,
+        }
+        converted_params["fc_dropout"] = {
+            "target": sigoptparams.fc_dropout_target,
+        }
 
     converted_details["lr"] = {
         "lr": sigoptparams.lr,
--- persite_painn/utils/wandb_utils.py
+++ persite_painn/utils/wandb_utils.py
@@ -7,24 +7,26 @@ def upload_artifact(name, filename, artifact_type):
     wandb.run.log_artifact(artifact)
 
 
-def save_artifacts(savedir):
+def save_artifacts(savedir, multifidelity):
     model_filename = savedir + "/best_model.pth.tar"
     test_ids_filename = savedir + "/test_ids.pkl"
     test_preds_filename = savedir + "/test_preds.pkl"
     test_targs_filename = savedir + "/test_targs.pkl"
-    test_preds_fidelity_filename = savedir + "/test_preds_fidelity.pkl"
-    test_targs_fidelity_filename = savedir + "/test_targs_fidelity.pkl"
+    if multifidelity:
+        test_preds_fidelity_filename = savedir + "/test_preds_fidelity.pkl"
+        test_targs_fidelity_filename = savedir + "/test_targs_fidelity.pkl"
     upload_artifact("model", model_filename, artifact_type="model")
     upload_artifact("test_ids", test_ids_filename, artifact_type="test_ids")
     upload_artifact("test_preds", test_preds_filename, artifact_type="test_preds")
     upload_artifact("test_targs", test_targs_filename, artifact_type="test_targs")
-    upload_artifact(
-        "test_preds_fidelity",
-        test_preds_fidelity_filename,
-        artifact_type="test_preds_fidelity",
-    )
-    upload_artifact(
-        "test_targs_fidelity",
-        test_targs_fidelity_filename,
-        artifact_type="test_targs_fidelity",
-    )
+    if multifidelity:
+        upload_artifact(
+            "test_preds_fidelity",
+            test_preds_fidelity_filename,
+            artifact_type="test_preds_fidelity",
+        )
+        upload_artifact(
+            "test_targs_fidelity",
+            test_targs_fidelity_filename,
+            artifact_type="test_targs_fidelity",
+        )
