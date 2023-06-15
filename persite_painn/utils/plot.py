bad1f3ec715b8e52b9d0e4e8c9e516aa30a8eeef
HojeChun <hoje.chun316@gmail.com>
2023-06-15 13:39:25 +0900

    plotting

--- persite_painn/utils/plot.py
+++ persite_painn/utils/plot.py
@@ -43,7 +43,7 @@ PROPERTIES = {
     "tot_e": "Energy per atom",
     "time": "Runtime",
     "magmom": "Magnetic moment",
-    "ads_e": "Adsorption energy",
+    "ads_e": "$E_{b}$",
     "acid_stab": "Electrochemical stability",
     "bandcenter": "DOS band center",
     "bandwidth": "DOS band width",
@@ -69,6 +69,29 @@ UNITS = {
     "bader": "$q_e$"
 }
 
+def set_size(num_col, ax=None):
+    """ w, h: width, height in inches """
+    if num_col == 1:
+        w = 5.6
+        h = 5.6
+    elif num_col == 2:
+        w = 2.8
+        h = 2.8
+    elif num_col == 3:
+        w = 1.867
+        h = 1.867
+    elif num_col == 4:
+        w = 1.4
+        h = 1.4
+    if not ax: ax=plt.gca()
+    l = ax.figure.subplotpars.left
+    r = ax.figure.subplotpars.right
+    t = ax.figure.subplotpars.top
+    b = ax.figure.subplotpars.bottom
+    figw = float(w)/(r-l)
+    figh = float(h)/(t-b)
+    ax.figure.set_size_inches(figw, figh)
+
 
 def plot_scatter(
     targs,
@@ -79,6 +102,7 @@ def plot_scatter(
     structures: Optional[Dict[int, Structure]] = None,
     Z_range: Optional[List] = None,
     title="",
+    num_col:int =2,
     scale="linear",
     inc_factor=1.1,
     dec_factor=0.9,
@@ -141,7 +165,17 @@ def plot_scatter(
 
     with plt.style.context(style):
         fig, ax = plt.subplots()
-        ax.scatter(new_targ, new_pred, facecolors=facecolor, edgecolors=edgecolor, alpha=0.75, linewidth=0.5, s=15)
+        if num_col ==4:
+            ax.scatter(new_targ, new_pred, facecolors=facecolor, edgecolors=edgecolor, alpha=0.75, linewidth=0.5, s=12)
+        else:
+            ax.scatter(new_targ, new_pred, facecolors=facecolor, edgecolors=edgecolor, alpha=0.75, linewidth=0.5, s=15)
+        set_size(num_col=num_col,ax=ax)
+        # if num_col==4:
+        #     print(fig.get_size_inches())
+        #     set_size(num_col=2, ax=ax)
+        #     print(fig.get_size_inches())
+        #     fig.set_size_inches(fig.get_size_inches()*0.625)
+        #     print(fig.get_size_inches())
         mae = mean_absolute_error(new_targ, new_pred)
         r, _ = pearsonr(new_targ, new_pred)
         r_s = spearmanr(new_targ, new_pred).correlation
@@ -173,19 +207,30 @@ def plot_scatter(
         ax.set_ylim(lim_min, lim_max)
         ax.set_xticks(ax.get_yticks())
         ax.set_yticks(ax.get_yticks())
+        if num_col == 4:
+            ax.tick_params(axis='both', labelsize=6)
         ax.set_aspect("equal")
 
         ax.axline((0, 0), (1, 1), color="#000000", zorder=-1, linewidth=0.5)
         ax.set_title(title, fontsize=8)
-        ax.set_ylabel("Predicted %s [%s]" % (PROPERTIES[prop_key], UNITS[prop_key]), fontsize=8)
-        ax.set_xlabel("Calculated %s [%s]" % (PROPERTIES[prop_key], UNITS[prop_key]), fontsize=8)
-
-        ax.annotate(
-            "Pearson's r: %.3f \nSpearman's r: %.3f \nMAE: %.3f %s " % (r, r_s, mae, UNITS[prop_key]),
-            (0.05, 0.85),
-            xycoords="axes fraction",
-            fontsize=6,
-        )
+        if num_col==4:
+            ax.set_ylabel("Predicted %s (%s)" % (PROPERTIES[prop_key], UNITS[prop_key]), fontsize=7, labelpad=3)
+            ax.set_xlabel("Calculated %s (%s)" % (PROPERTIES[prop_key], UNITS[prop_key]), fontsize=7, labelpad=3)
+            ax.annotate(
+                "Pearson's r: %.3f \nSpearman's r: %.3f \nMAE: %.3f %s " % (r, r_s, mae, UNITS[prop_key]),
+                (0.05, 0.775),
+                xycoords="axes fraction",
+                fontsize=5,
+            )
+        else:
+            ax.set_ylabel("Predicted %s (%s)" % (PROPERTIES[prop_key], UNITS[prop_key]), fontsize=8)
+            ax.set_xlabel("Calculated %s (%s)" % (PROPERTIES[prop_key], UNITS[prop_key]), fontsize=8)
+            ax.annotate(
+                "Pearson's r: %.3f \nSpearman's r: %.3f \nMAE: %.3f %s " % (r, r_s, mae, UNITS[prop_key]),
+                (0.05, 0.8),
+                xycoords="axes fraction",
+                fontsize=6,
+            )
 
     return fig, ax, r, mae
 
@@ -199,6 +244,7 @@ def plot_hexbin(
     structures: Optional[Dict[int, Structure]] = None,
     Z_range: Optional[List] = None,
     title="",
+    num_col:int =2,
     scale="linear",
     inc_factor=1.1,
     dec_factor=0.9,
@@ -345,14 +391,23 @@ def plot_hexbin(
         ax.set_title(title, fontsize=8)
         ax.set_ylabel("Predicted %s [%s]" % (PROPERTIES[prop_key], UNITS[prop_key]), fontsize=8)
         ax.set_xlabel("Calculated %s [%s]" % (PROPERTIES[prop_key], UNITS[prop_key]), fontsize=8)
-
-        ax.annotate(
-            "Pearson's r: %.3f \nSpearman's r: %.3f \nMAE: %.3f %s " % (r, r_s, mae, UNITS[prop_key]),
-            (0.05, 0.85),
-            xycoords="axes fraction",
-            fontsize=6,
-        )
-
+        # ax.set_yticks(ax.get_yticks())
+        ax.set_aspect("equal")
+        if num_col == 4:
+            ax.annotate(
+                "Pearson's r: %.3f \nSpearman's r: %.3f \nMAE: %.3f %s " % (r, r_s, mae, UNITS[prop_key]),
+                (0.05, 0.75),
+                xycoords="axes fraction",
+                fontsize=6,
+            )
+        else:
+            ax.annotate(
+                "Pearson's r: %.3f \nSpearman's r: %.3f \nMAE: %.3f %s " % (r, r_s, mae, UNITS[prop_key]),
+                (0.05, 0.85),
+                xycoords="axes fraction",
+                fontsize=6,
+            )
+        set_size(num_col=num_col, ax=ax)
     return fig, ax, r, mae, hb
 
 
@@ -367,6 +422,7 @@ def plot_violin(
     prop_key: str,
     test_ids: List,
     structures: Structure,
+    num_col: int =2,
     color1: str = "#679289",
     color2: str = "#F4C095",
     Z_range=[22, 30],
@@ -423,11 +479,11 @@ def plot_violin(
 
         ys.append(dictionary[Z])
         Zs.append([Z for i in range(len(dictionary[Z]))])
-        hues.append(["targ" for i in range(len(dictionary[Z]))])
+        hues.append(["calculated" for i in range(len(dictionary[Z]))])
 
         ys.append(pred_dictionary[Z])
         Zs.append([Z for i in range(len(pred_dictionary[Z]))])
-        hues.append(["pred" for i in range(len(pred_dictionary[Z]))])
+        hues.append(["predicted" for i in range(len(pred_dictionary[Z]))])
 
     Zs = flatten(Zs)
     ys = flatten(ys)
@@ -437,7 +493,7 @@ def plot_violin(
     df[prop_key] = ys
     df["hue"] = hues
 
-    my_pal = {"targ": color1, "pred": color2}
+    my_pal = {"calculated": color1, "predicted": color2}
     with plt.style.context(style):
         f = plt.figure()
         ax = sns.violinplot(
@@ -448,21 +504,25 @@ def plot_violin(
             palette=my_pal,
             split=True,
             inner=None,
-            linewidth=1,
+            linewidth=0.5,
+            alpha=0.5
         )
 
         # plt.ylim([-0.1,5.2])'
 
+        set_size(num_col=num_col,ax=ax)
         plt.legend(loc=legend_loc, fontsize=6)
-        ax.set_ylabel("%s [%s]" % (PROPERTIES[prop_key], UNITS[prop_key]), fontsize=8)
+        if num_col == 4:
+            ax.set_ylabel("%s (%s)" % (PROPERTIES[prop_key], UNITS[prop_key]), fontsize=7, labelpad=3)
+        else:
+            ax.set_ylabel("%s (%s)" % (PROPERTIES[prop_key], UNITS[prop_key]), fontsize=8)
         ax.set_xlabel("")
-        ax.tick_params(axis='x',labelsize=8)
+        ax.tick_params(axis='x',labelsize=7)
         ax.tick_params(axis='y',labelsize=6)
         xticks = [Element.from_Z(int(text._text)).symbol for text in ax.get_xticklabels()]
-        ax.set_xticklabels(xticks)
+        ax.set_xticklabels(xticks, fontsize=7)
         if not legend:
             ax.get_legend().remove()
-
     return f, ax
 
 
